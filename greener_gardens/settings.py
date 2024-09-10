d3d59946b9f1250a3d66a2d20aa52b2dd16035e4
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-09-10 18:28:05 +0000

    core: add heroku url to allowed hosts

--- greener_gardens/settings.py
+++ greener_gardens/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/greener-gardens.git', 'localhost']
 
 
 # Application definition
